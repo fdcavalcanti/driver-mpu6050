8d3b6263cf0c679d533489ba2af7e3d1cde7e2cd
Filipe Cavalcanti <filipedoocv@gmail.com>
2022-06-27 21:25:42 -0300

    Added a parameter to be passed in initialization: mpu i2c addr

--- driver/mpu6050.c
+++ driver/mpu6050.c
@@ -1,15 +1,20 @@
 #define pr_fmt(fmt) "%s %s: " fmt, KBUILD_MODNAME, __func__
 #include <linux/module.h>
 
+static int MPU_ADDR = 0x68;
 
 static int __init mpu_init(void) {
-  pr_info("Initializing MPU on\n");
+  pr_info("Initializing MPU on 0x%X", MPU_ADDR);
+
   return 0;
 }
 
 static void __exit mpu_exit(void) {
   pr_info("Removing MPU\n");
 }
+
+module_param(MPU_ADDR, int, S_IRUSR | S_IWUSR);
+MODULE_PARM_DESC(MPU_ADDR, "MPU I2C address.");
 
 module_init(mpu_init);
 module_exit(mpu_exit);
